eb01695f1549440049f311964ca7589bc72aae84
TanvirHridoy <anxioushridoy4@gmail.com>
2024-05-30 06:10:39 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Dependency Injection 
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//request pipeline middleware 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapGet("/GetMyName", () =>
+{
+    return Results.Ok("Hridoy");
+});
+
+
+app.Run();
+
